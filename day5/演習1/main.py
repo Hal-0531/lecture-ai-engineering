fd1084c006af952fe403d9dbc73f6a0fcce30c48
Hal-0531 <ibm.ktym@gmail.com>
2025-05-21 06:08:55 +0900

    Update main.py

--- day5/演習1/main.py
+++ day5/演習1/main.py
@@ -130,3 +130,21 @@ if __name__ == "__main__":
     with open(model_path, "wb") as f:
         pickle.dump(model, f)
     #print(f"モデルを {model_path} に保存しました")
+
+    file_path = sys.argv[1]
+    # ファイルからデータを読み取る
+    data_dict = {}
+    with open(file_path, "r") as f:
+        for line in f:
+            key, value = line.strip().split(": ")
+            data_dict[key] = float(value)  # 数値として扱う
+    
+    # 変数として扱う
+    pre_accuracy = data_dict["accuracy"]
+    pre_pre_time = data_dict["pre_time"]
+    changed_acc = accuracy - pre_accuracy
+    changed_pre = pre_time - pre_pre_time
+    with open("compare.txt", "w") as f:
+        f.write(f"Accuracy: {changed_acc:.4f}\n")
+        f.write(f"Pre Time: {changed_pre:.6f} sec\n")
+    
