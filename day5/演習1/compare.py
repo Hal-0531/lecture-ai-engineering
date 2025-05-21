4e94602010776966d77903e42c65e0cd7e783278
Hal-0531 <ibm.ktym@gmail.com>
2025-05-21 12:14:40 +0900

    Update compare.py

--- day5/演習1/compare.py
+++ day5/演習1/compare.py
@@ -8,10 +8,18 @@ with open(file_path, "r") as f:
         data_dict[key] = float(value)  # 数値として扱う
     
 # 変数として扱う
-pre_accuracy = data_dict["accuracy"]
-pre_pre_time = data_dict["pre_time"]
+pre_accuracy = data_dict["Accuracy"]
+pre_pre_time = data_dict["Pre Time"]
 changed_acc = accuracy - pre_accuracy
 changed_pre = pre_time - pre_pre_time
+if changed_acc < 0:
+    result = "モデルの性能が劣化しました"
+if changed_acc = 0:
+    result = "モデルの性能は変化しませんでした"
+else:
+    result = "モデルの性能が向上しました"
+
 with open("compare.txt", "w") as f:
     f.write(f"Accuracy: {changed_acc:.4f}\n")
     f.write(f"Pre Time: {changed_pre:.6f} sec\n")
+    f.write(f"結果: {result}\n")
