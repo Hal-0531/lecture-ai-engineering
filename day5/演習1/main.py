9e8ef3cc63b6b3723f38a65a329dca415b267b10
Hal-0531 <ibm.ktym@gmail.com>
2025-05-21 12:32:39 +0900

    Update main.py

--- day5/演習1/main.py
+++ day5/演習1/main.py
@@ -115,6 +115,31 @@ if __name__ == "__main__":
         max_depth=max_depth,
         random_state=model_random_state,
     )
+    import sys
+    file_path = sys.argv[1]
+    # ファイルからデータを読み取る
+    data_dict = {}
+    with open(file_path, "r") as f:
+        for line in f:
+            key, value = line.strip().split(": ")
+            data_dict[key] = float(value)  # 数値として扱う
+        
+    # 変数として扱う
+    pre_accuracy = data_dict["Accuracy"]
+    pre_pre_time = data_dict["Pre Time"]
+    changed_acc = accuracy - pre_accuracy
+    changed_pre = pre_time - pre_pre_time
+    if changed_acc < 0:
+        result = "モデルの性能が劣化しました"
+    elif changed_acc == 0:
+        result = "モデルの性能は変化しませんでした"
+    else:
+        result = "モデルの性能が向上しました"
+    
+    with open("compare.txt", "w") as f:
+        f.write(f"精度の変化: {changed_acc:.4f}\n")
+        f.write(f"推論時間の変化: {changed_pre:.6f} sec\n")
+        f.write(f"結果: {result}\n")
     # 結果を保存
     with open("result.txt", "w") as f:
         f.write(f"Accuracy: {accuracy:.4f}\n")
